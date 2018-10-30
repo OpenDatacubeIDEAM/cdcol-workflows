1bae225146d3ce5cc7384ca82f458f319495f3b8
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2018-10-30 08:22:36 -0500

    Update delete_dags_from_database.sql

--- utils/delete_dags_from_database.sql
+++ utils/delete_dags_from_database.sql
@@ -1,9 +1,10 @@
-
 create temporary table t_dags_to_remove as select dag_id from dag where dag_id like '%something%';
 delete from xcom where dag_id in (select * from t_dags_to_remove);
 delete from task_instance where dag_id in (select * from t_dags_to_remove);
+delete from task_fail where dag_id in (select * from t_dags_to_remove);
 delete from sla_miss where dag_id in (select * from t_dags_to_remove);
 delete from log where dag_id in (select * from t_dags_to_remove);
 delete from job where dag_id in (select * from t_dags_to_remove);
 delete from dag_run where dag_id in (select * from t_dags_to_remove);
+delete from dag_stats where dag_id in (select * from t_dags_to_remove);
 delete from dag where dag_id in (select * from t_dags_to_remove);
