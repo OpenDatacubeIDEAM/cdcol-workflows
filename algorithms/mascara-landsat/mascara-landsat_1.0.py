473eeb4094fba84cf33a1a8494a25716a20ec74b
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2019-02-06 13:47:59 -0500

    actualizando el algoritmo de compuesto de medianas y de mascara

--- algorithms/mascara-landsat/mascara-landsat_1.0.py
+++ algorithms/mascara-landsat/mascara-landsat_1.0.py
@@ -8,14 +8,12 @@ nodata=-9999
 validValues=set()
 if product=="LS7_ETM_LEDAPS" or product == "LS5_TM_LEDAPS":
     validValues=[66,68,130,132]
-    calculate_mask(validValues)
 elif product == "LS8_OLI_LASRC":
     validValues=[322, 386, 834, 898, 1346, 324, 388, 836, 900, 1348]
-    calculate_mask(validValues)
 else:
     raise Exception("Este algoritmo sólo puede enmascarar LS7_ETM_LEDAPS, LS5_TM_LEDAPS o LS8_OLI_LASRC")
 
 cloud_mask = np.isin(xarr0["pixel_qa"].values, validValues)
-    for band in bands:
-        xarr0[band].values = np.where(np.logical_and(xarr0.data_vars[band] != nodata, cloud_mask),xarr0.data_vars[band], nodata).astype(np.int16)
-    output = xarr0
+for band in bands:
+    xarr0[band].values = np.where(np.logical_and(xarr0.data_vars[band] != nodata, cloud_mask), xarr0.data_vars[band],nodata).astype(np.int16)
+output = xarr0
