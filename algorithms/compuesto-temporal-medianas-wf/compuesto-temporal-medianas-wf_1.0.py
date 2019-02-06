cb311fa4455bc970b89999506ec6546353bf9fef
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2019-02-06 11:50:20 -0500

    actualizando el algoritmo de compuesto de medianas y de mascara

--- algorithms/compuesto-temporal-medianas-wf/compuesto-temporal-medianas-wf_1.0.py
+++ algorithms/compuesto-temporal-medianas-wf/compuesto-temporal-medianas-wf_1.0.py
@@ -1,40 +1,27 @@
+#!/usr/bin/python3
+# coding=utf8
 import xarray as xr
 import numpy as np
-print "Excecuting medianas v2"
-medians={}
-nodata=-9999
-#Se hace un recorrido sobre las bandas
+print ("Compuesto temporal de medianas para " + product)
+medians = {}
 for band in bands:
-    #Los datos se reciben enmascarados
-    # 
-    #np.isnan verifica que valores no son numero o no estan definidos. En este caso, se usa ~ para invertir los valores y saber que valores de datos si estan definidos
-    datos=xarr0.data_vars[band].values
-    allNan=~np.isnan(datos)
-    # if normalized:
-    #     #Calcula la mediana arimetica sobre el arreglo de datos  ,sobre el eje  , ignorando los valores NaN
-    #     m=np.nanmean(datos.reshape((datos.shape[0],-1)), axis=1)
-    #     #Calcula la desviacion estandar sobre el arreglo de datos  , sobre el eje , ignorando los valores NaN
-    #     st=np.nanstd(datos.reshape((datos.shape[0],-1)), axis=1)
-    #     datos=np.true_divide((datos-m[:,np.newaxis,np.newaxis]), st[:,np.newaxis,np.newaxis])
-    #Calcula la mediana aritmetica 
-    medians[band]=np.nanmedian(datos,0).astype(np.int16)
-    medians[band][np.sum(allNan,0)<minValid]=nodata
-#Elimina la variable datos y la asociacion que tiene en el algoritmo
+    datos = xarr0.data_vars[band]
+    allNan = ~np.isnan(datos)
+    medians[band] = np.nanmedian(datos, 0)
+    medians[band][np.sum(allNan, 0) < minValid] = np.nan
 del datos
 
-import xarray as xr
+
+# > **Asignación de coordenadas**
 ncoords=[]
 xdims =[]
 xcords={}
-for x in xarr0.coords:
+for x in nbar.coords:
     if(x!='time'):
-        ncoords.append( ( x, xarr0.coords[x]) )
+        ncoords.append( ( x, nbar.coords[x]) )
         xdims.append(x)
-        xcords[x]=xarr0.coords[x]
-variables ={k: xr.DataArray(v, dims=xdims,coords=ncoords)
-            for k, v in medians.items()}
-#Genera el dataset (netcdf) con las bandas con el sistema de referencia de coordenadas
-output=xr.Dataset(variables, attrs={'crs':xarr0.crs})
-
+        xcords[x]=nbar.coords[x]
+variables ={k: xr.DataArray(v, dims=xdims,coords=ncoords) for k, v in medians.items()}
+output=xr.Dataset(variables, attrs={'crs':nbar.crs})
 for x in output.coords:
-    output.coords[x].attrs["units"]=xarr0.coords[x].units
+    output.coords[x].attrs["units"]=nbar.coords[x].units
--- algorithms/mascara-landsat/mascara-landsat_1.0.py
+++ algorithms/mascara-landsat/mascara-landsat_1.0.py
@@ -1,36 +1,24 @@
-#Requiere que el producto sea LS7_ETM_LEDAPS, LS5_TM_LEDAPS o LS8_OLI_LASRC
-import xarray as xr
+#!/usr/bin/python3
+# coding=utf8
+
 import numpy as np
-print "Masking "+product
-
-#La funcion isin permite verificar si el elemento (elemento) que se pasa por parametro está dentro del arreglo (test_elements)
-#Esta funcion se define para verificar los valores validos de la mascara de nube (pixel_qa) 
-def isin(element, test_elements, assume_unique=False, invert=False):
-    "definiendo la función isin de numpy para la versión anterior a la 1.13, en la que no existe"
-    element = np.asarray(element)
-    return np.in1d(element, test_elements, assume_unique=assume_unique,
-                invert=invert).reshape(element.shape)
-#Un valor negativo suficientemente grande para representar cuando no hay datos
+print ("Masking " + product)
 nodata=-9999
-medians={}
 
-#Conjunto de valores valido para la mascara de nube
+def calculate_mask(valid_values):
+    cloud_mask = np.isin(nbar["pixel_qa"].values, validValues)
+    for band in bands:
+        xarr0[band].values = np.where(np.logical_and(xarr0.data_vars[band] != nodata, cloud_mask),xarr0.data_vars[band], nodata).astype(np.int16)
+    output = xarr0
+
 validValues=set()
-
-#Dependiendo del satellite, se definen unos valores distintos para la mascara
-#Estos valores se definen en la página de LandSat: https://landsat.usgs.gov/landsat-surface-reflectance-quality-assessment
-#Son valores validos porque representan agua (water) o un "pixel claro" (clear)
 if product=="LS7_ETM_LEDAPS" or product == "LS5_TM_LEDAPS":
     validValues=[66,68,130,132]
+    calculate_mask(validValues)
 elif product == "LS8_OLI_LASRC":
     validValues=[322, 386, 834, 898, 1346, 324, 388, 836, 900, 1348]
+    calculate_mask(validValues)
+elif product=='LS7_ETM_LEDAPS_MOSAIC':
+    output = xarr0
 else:
     raise Exception("Este algoritmo sólo puede enmascarar LS7_ETM_LEDAPS, LS5_TM_LEDAPS o LS8_OLI_LASRC")
-
-cloud_mask=isin(xarr0["pixel_qa"].values, validValues)
-for band in bands:
-    # np.where es la funcion where de la libreria numpy que retorna un arreglo o un set de arreglos con datos que cumplen con la condicion dada por parametro
-    # 
-    xarr0[band].values = np.where(np.logical_and(xarr0.data_vars[band] != nodata, cloud_mask), xarr0.data_vars[band],nodata).astype(np.int16)
-
-output=xarr0
