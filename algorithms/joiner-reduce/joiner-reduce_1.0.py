7d39108f3e9008d9a62094f8e4a0c62e121fdc32
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2019-02-19 12:37:19 -0500

    modificando una vez más el joiner reduce

--- algorithms/joiner-reduce/joiner-reduce_1.0.py
+++ algorithms/joiner-reduce/joiner-reduce_1.0.py
@@ -4,19 +4,19 @@
 import xarray as xr
 output=None
 xarrs=list(xarrs.values())
-try:
-    bands
-except NameError:
-    bands=xarrs[0].keys()
-for _xarr in xarrs:
-    _undesired=list(set(_xarr.keys())-set(bands+['latitude','longitude','time']))
-    _xarr=_xarr.drop(_undesired)
-    if output is None:
-        output = _xarr
-    else:
-        output=xr.concat([output,_xarr.copy(deep=True)],'time')
-
-output['crs'] = xarrs[0].crs
-output=output.transpose(*xarrs[0].coords.keys())
-# output=xr.merge(xarrs, compat='no_conflicts',join='inner')
+# try:
+#     bands
+# except NameError:
+#     bands=xarrs[0].keys()
+# for _xarr in xarrs:
+#     _undesired=list(set(_xarr.keys())-set(bands+['latitude','longitude','time']))
+#     _xarr=_xarr.drop(_undesired)
+#     if output is None:
+#         output = _xarr
+#     else:
+#         output=xr.concat([output,_xarr.copy(deep=True)],'time')
+#
+# output['crs'] = xarrs[0].crs
+# output=output.transpose(*xarrs[0].coords.keys())
+output=xr.merge(xarrs, compat='no_conflicts',join='inner')
 print(output.coords['time'])
