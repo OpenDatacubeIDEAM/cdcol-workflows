580066e50db1269972bbc88e891b0634f1060bfe
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2019-02-19 12:50:35 -0500

    modificando una vez más el joiner reduce

--- algorithms/joiner-reduce/joiner-reduce_1.0.py
+++ algorithms/joiner-reduce/joiner-reduce_1.0.py
@@ -11,12 +11,12 @@ except NameError:
 for _xarr in xarrs:
     _undesired=list(set(_xarr.keys())-set(bands+['latitude','longitude','time']))
     _xarr=_xarr.drop(_undesired)
-#     if output is None:
-#         output = _xarr
-#     else:
-#         output=xr.concat([output,_xarr.copy(deep=True)],'time')
-#
-# output['crs'] = xarrs[0].crs
+    if output is None:
+        output = _xarr
+    else:
+        output=xr.merge([output,_xarr.copy(deep=True)], compat='no_conflicts',join='inner')
+
+output['crs'] = xarrs[0].crs
 # output=output.transpose(*xarrs[0].coords.keys())
-output=xr.merge(xarrs, compat='no_conflicts',join='inner')
+#output=xr.merge(xarrs, compat='no_conflicts',join='inner')
 print(output.coords['time'])
