bc5991ab58bedbc4e21bdb00a17902e687c458ef
MariaMancipe <paula.mancipe@bcomcolombia.com>
2019-02-21 08:14:42 -0500

    (nuevo) agregando el bash operator al init

--- plugins/cdcol_plugin/__init__.py
+++ plugins/cdcol_plugin/__init__.py
@@ -3,6 +3,7 @@ from airflow.plugins_manager import AirflowPlugin
 from cdcol_plugin.operators.query_operator import CDColQueryOperator
 from cdcol_plugin.operators.from_file_operator import CDColFromFileOperator
 from cdcol_plugin.operators.reduce_operator import CDColReduceOperator
+from cdcol_plugin.operators.bash_executor_operator import CDColBashOperator
  
  
 class CDColPlugin(AirflowPlugin):
