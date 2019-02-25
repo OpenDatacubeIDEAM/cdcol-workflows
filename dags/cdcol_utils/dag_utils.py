f7bf96106e401118435b6522e19977ba43f417ce
MariaMancipe <paula.mancipe@bcomcolombia.com>
2019-02-25 12:47:43 -0500

    modificación en el utils y en ndvi

--- dags/cdcol_utils/dag_utils.py
+++ dags/cdcol_utils/dag_utils.py
@@ -53,8 +53,9 @@ def queryMapByTileByMonths(lat,lon,time_ranges,queue, dag, algorithm,version,par
 def IdentityMap(upstream,algorithm,version, queue, dag, taxprefix,params={}):
     i=1
     tasks=[]
+    trans = str.maketrans({"(": None, ")": None, " ": None, ",": "-"})
     for prev in upstream:
-        _t=CDColFromFileOperator(algorithm=algorithm,version=version,queue=queue, dag=dag,  lat=prev.lat, lon=prev.lon, task_id="{}_{}_{}".format(taxprefix,prev.lat,prev.lon ),params=params)
+        _t=CDColFromFileOperator(algorithm=algorithm,version=version,queue=queue, dag=dag,  lat=prev.lat, lon=prev.lon, task_id=("{}_{}_{}".format(taxprefix,prev.lat,prev.lon)).translate(trans),params=params)
         i+=1
         prev>>_t
         tasks.append(_t)
@@ -64,8 +65,9 @@ def IdentityMap(upstream,algorithm,version, queue, dag, taxprefix,params={}):
 def BashMap(upstream,algorithm,version, queue, dag, taxprefix,params={}):
     i = 1
     tasks = []
+    trans = str.maketrans({"(": None, ")": None, " ": None, ",": "-"})
     for prev in upstream:
-        _t = CDColBashOperator(algorithm=algorithm, version=version, queue=queue, dag=dag, lat=prev.lat, lon=prev.lon, task_id="{}_{}_{}".format(taxprefix,prev.lat,prev.lon ), params=params)
+        _t = CDColBashOperator(algorithm=algorithm, version=version, queue=queue, dag=dag, lat=prev.lat, lon=prev.lon, task_id=("{}_{}_{}".format(taxprefix,prev.lat,prev.lon)).translate(trans), params=params)
         i += 1
         prev >> _t
         tasks.append(_t)
