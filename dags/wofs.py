1746f6f553f3a0f5a11f9d8af35f634441b3b7de
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2019-02-27 13:56:36 -0500

    (modificado) deteccion_cambios_pca

--- dags/wofs.py
+++ dags/wofs.py
@@ -11,7 +11,7 @@ from pprint import pprint
 _params = {
     'lat': (4, 6),
     'lon': (-74, -72),
-    'time_ranges': ("2017-01-01", "2017-12-31"),
+    'time_ranges': ("2015-01-01", "2017-12-31"),
     'bands': ["blue", "green", "red", "nir", "swir1", "swir2", "pixel_qa"],
     'minValid': 1,
     'normalized': True,
