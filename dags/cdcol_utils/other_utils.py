448cf16065a8943e978fb092784724dd71472541
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2019-06-10 16:43:13 -0500

    si no hay nada, no generar .zip

--- dags/cdcol_utils/other_utils.py
+++ dags/cdcol_utils/other_utils.py
@@ -24,13 +24,14 @@ def compress_results(execID,**kwargs):
     dag_results_folder = "{}/{}/".format(common.RESULTS_FOLDER, execID)
     if os.path.exists(dag_results_folder) and os.path.isdir(dag_results_folder) and len(os.listdir(dag_results_folder))>0:
             for folder, subfolders, files in os.walk(dag_results_folder):
-                print(files)
-                print(len(files))
                 for subfolder in subfolders:
                     if len(os.listdir(os.path.join(folder,subfolder)))==0:
                         shutil.rmtree(os.path.join(folder,subfolder), ignore_errors=True)
-                with zipfile.ZipFile(os.path.join(dag_results_folder, "resultados_{}.zip".format(execID)),"w") as file_to_compress:
-                    for file in files:
-                        file_to_compress.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder,file), dag_results_folder), compress_type = zipfile.ZIP_DEFLATED)
-                file_to_compress.close()
+                if len(files)>0:
+                    with zipfile.ZipFile(os.path.join(dag_results_folder, "resultados_{}.zip".format(execID)),"w") as file_to_compress:
+                        for file in files:
+                            file_to_compress.write(os.path.join(folder, file), os.path.relpath(os.path.join(folder,file), dag_results_folder), compress_type = zipfile.ZIP_DEFLATED)
+                    file_to_compress.close()
+                    return os.path.join(dag_results_folder, "resultados_{}.zip".format(execID))
+
 
