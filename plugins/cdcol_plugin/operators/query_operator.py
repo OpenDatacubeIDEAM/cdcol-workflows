b1a3e8ff07b1b057aa2abb3ac6980a652267426d
MariaMancipe <mp.mancipe10@uniandes.edu.co>
2019-06-09 20:42:27 -0500

    no hay datos en la zona

--- plugins/cdcol_plugin/operators/query_operator.py
+++ plugins/cdcol_plugin/operators/query_operator.py
@@ -65,7 +65,7 @@ class CDColQueryOperator(BaseOperator):
                                                 latitude=self.lat, time=t)
                 if len(kwargs[xanm + str(i)].data_vars) == 0:
                     print("ERROR: No hay datos en la zona")
-                    open(folder + "{}_{}_no_data.lock".format(self.lon[0], self.lat[0]), "w+").close()
+                    open(folder + "no_data_{}_{}.lock".format(self.lon[0], self.lat[0]), "w+").close()
                     raise AirflowSkipException("No hay datos en la zona")
                 i += 1
         else:
@@ -73,7 +73,7 @@ class CDColQueryOperator(BaseOperator):
                                             latitude=self.lat, time=self.time_ranges)
             if len(kwargs[xanm + str(0)].data_vars) == 0:
                 print("ERROR: No hay datos en la zona")
-                open(folder + "{}_{}_no_data.lock".format(self.lon[0], self.lat[0]), "w+").close()
+                open(folder + "no_data_{}_{}.lock".format(self.lon[0], self.lat[0]), "w+").close()
                 raise AirflowSkipException("No hay datos en la zona")
         # kwargs[xanm] = dc.load(product=self.product['name'], longitude=self.lon, latitude=self.lat, time=self.time_ranges)
 
