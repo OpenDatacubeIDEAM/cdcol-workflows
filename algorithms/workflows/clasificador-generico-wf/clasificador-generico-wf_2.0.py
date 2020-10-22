f0b344cca29bc4778f5a270da4c3923d46e2ca84
kill4n <kill4n@users.noreply.github.com>
2020-10-22 08:57:18 -0500

    Update clasificador-generico-wf_1.0
    Update clasificador-generico-wf_1.0

---
+++ algorithms/workflows/clasificador-generico-wf/clasificador-generico-wf_2.0.py
@@ -0,0 +1,195 @@
+# In[7]:
+import xarray as xr
+import numpy as np
+from sklearn.externals import joblib
+import warnings
+
+# In[21]:
+print('xarrs.keys')
+print(xarrs.keys())
+
+# Preprocesar:
+nmed=None
+nan_mask=None
+xarrs=list(xarrs.values())
+print(type(xarrs))
+medians1 = xarrs[0]
+print(type(medians1))
+print("medianas")
+bands_data = []
+
+print('medians1.datavars',medians1.data_vars.keys())
+
+#rows, cols = xarr0[product['bands'][0]].shape
+
+#print('rows',rows)
+#print('cols',cols)
+
+
+#for band in product['bands']:
+#    # pixel_qa is removed from xarr0 by Compuesto Temporal de Medianas
+#    if band != 'pixel_qa':
+#        bands_data.append(xarr0[band])
+#bands_data = np.dstack(bands_data)
+
+###test 2
+bands_data=[]
+
+bands2=list(medians1.data_vars.keys())
+print('bands2',bands2)
+#print('mediansband',medians1[band])
+
+for band in bands2: 
+	if band != 'pixel_qa':
+   	 bands_data.append(medians1[band])
+bands_data = np.dstack(bands_data)
+
+print('bands_data_test2',bands_data)
+
+
+rows, cols, n_bands = bands_data.shape
+
+print('rows',rows)
+print('cols',cols)
+
+print('n_bands',n_bands)
+
+
+
+print('fin test2')
+
+print('inicio test3')
+
+n_samples = rows*cols
+flat_pixels = bands_data.reshape((n_samples, n_bands))
+#mascara valores nan por valor no data
+mask_nan=np.isnan(flat_pixels)
+flat_pixels[mask_nan]=-9999
+#result = bagging_clf.predict(flat_pixels)
+#classification = result.reshape((rows, cols))
+
+
+
+
+
+
+print('fin test3')
+
+#for band in medians1.data_vars.keys():
+
+#    if band == "crs":
+#        continue
+#    b=np.ravel(medians1.data_vars[band].values)
+#    if nan_mask is None:
+#        nan_mask=np.isnan(b)
+#    else:
+#        nan_mask=np.logical_or(nan_mask, np.isnan(medians1.data_vars[band].values.ravel()))
+#    b[np.isnan(b)]=np.nanmedian(b)
+#    if nmed is None:
+#        sp=medians1.data_vars[band].values.shape
+#        nmed=b
+#    else:
+#        nmed=np.vstack((nmed,b))
+
+#print('mediansdata',
+#	sp=medians1.data_vars[band].values.shape
+#	bands_data.append(medians1[band])
+#bands_data = np.dstack(bands_data)
+
+#print('medians1.datavars',sp)
+
+#rows, cols = sp
+
+#print('rows',rows)
+#print('cols',cols)
+
+#print('bands',bands)
+
+#for band in bands: 
+#    bands_data.append(medians1[band])
+#bands_data = np.dstack(bands_data)
+
+#print('bands',bands_data)
+
+#n_samples = rows*cols
+#flat_pixels = bands_data.reshape((n_samples, n_bands))
+#mascara valores nan por valor no data
+#mask_nan=np.isnan(flat_pixels)
+#flat_pixels[mask_nan]=-9999
+#result = bagging_clf.predict(flat_pixels)
+#classification = result.reshape((rows, cols))
+
+
+# In[12]:
+
+import os
+
+print("modelo")
+
+model = None
+for file in other_files:
+    if file.endswith(".pkl"):
+        model = file
+        break
+if model is None:
+    raise "Debería haber un modelo en la carpeta " + modelos
+
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=UserWarning)
+    bagging_clf = joblib.load(model)
+    print(bagging_clf)
+
+print(bagging_clf)
+print("clasificacion final")
+result = bagging_clf.predict(flat_pixels)
+result = result.reshape((rows, cols))
+print("fin funcion de clasificacion")
+
+model=bagging_clf()
+model.fit(training_samples, training_labels)
+print(model.feature_importances_)
+
+#result = bagging_clf.predict(nmed.T)
+#result = result.reshape(sp)
+
+# In[ ]:
+
+
+# In[24]:
+
+#coordenadas = []
+#dimensiones = []
+#xcords = {}
+#for coordenada in xarrs[0].coords:
+#    if (coordenada != 'time'):
+#        coordenadas.append((coordenada, xarrs[0].coords[coordenada]))
+#        dimensiones.append(coordenada)
+#        xcords[coordenada] = xarrs[0].coords[coordenada]
+
+#valores = {"classified": xr.DataArray(result, dims=dimensiones, coords=coordenadas)}
+#array = xr.DataArray(result, dims=dimensiones, coords=coordenadas)
+#array.astype('float32')
+#valores = {"classified": array}
+
+#output = xr.Dataset(valores, attrs={'crs': xarrs[0].crs})
+#for coordenada in output.coords:
+#    output.coords[coordenada].attrs["units"] = xarrs[0].coords[coordenada].units
+
+#classified = output.classified
+#classified.values = classified.values.astype('float32')
+
+
+print(result.dtype)
+ncoords=[]
+xdims =[]
+xcords={}
+for x in xarr0.coords:
+    if(x!='time'):
+        ncoords.append( ( x, xarr0.coords[x]) )
+        xdims.append(x)
+        xcords[x]=xarr0.coords[x]
+variables ={"classified": xr.DataArray(result, dims=xdims,coords=ncoords)}
+output=xr.Dataset(variables, attrs={'crs':xarr0.crs})
+for x in output.coords:
+    output.coords[x].attrs["units"]=xarr0.coords[x].units
+
